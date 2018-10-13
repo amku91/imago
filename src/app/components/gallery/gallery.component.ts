e5964754da456ff5e924598086d5ed2a4f796663
amku91 <amit.kumar@wellthy.care>
2018-10-13 15:20:34 +0530

    Pagination with 5 column layout added

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -3,9 +3,8 @@ import { Router } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Observable, empty, of } from 'rxjs';
 import { environment } from '../../../environments/environment';
-
 import { ImageService } from '../../services/image/image.service';
-
+import { PageEvent } from '@angular/material';
 import 'rxjs/add/observable/interval';
 
 @Component({
@@ -15,8 +14,14 @@ import 'rxjs/add/observable/interval';
 })
 export class GalleryComponent implements OnInit {
 
-  preImageData:any = [];
-  newUploadedImageData:any = [];
+  preImageData: any = [];
+  allImageData: any = [];
+  newUploadedImageData: any = [];
+  length: number = 0;
+  pageSize: number = 15;
+  pageSizeOptions: number[] = [10, 15, 25, 50];
+  pageEvent: PageEvent;
+  activePageData: any = [];
   constructor(public imageService: ImageService) { }
 
   ngOnInit() {
@@ -24,13 +29,41 @@ export class GalleryComponent implements OnInit {
     this.showPreImages();
   }
 
-  showPreImages(){
+  showPreImages() {
     this.imageService.getPreImages().subscribe(data => {
-      let rowData:any = data;
+      let rowData: any = data;
       this.preImageData = rowData.resources;
+      this.allImageData = this.getAllImageData(rowData.resources);
+      this.length = this.allImageData.length;
+      this.activePageData = this.getGridLayoutData(this.allImageData.slice(0, this.pageSize));
     }, error => {
-      this.imageService.showSnackBar("Unable to load pre images", "Ok");
+      this.imageService.showSnackBar("Unable to load pre images.", "Ok");
     });
+  }
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  }
+
+  onPageChanged(e) {
+    let firstCut = e.pageIndex * e.pageSize;
+    let secondCut = firstCut + e.pageSize;
+    this.activePageData = this.getGridLayoutData(this.allImageData.slice(firstCut, secondCut));
+  }
+
+  getAllImageData(preImageData: any) {
+    return preImageData;
+  }
+
+  getGridLayoutData(rowData: any) {
+    let result: any = [];
+    let i: number;
+    let j: number;
+    let chunk: number = 5;
+    for (i = 0, j = rowData.length; i < j; i += chunk) {
+      let temparray = rowData.slice(i, i + chunk);
+      result.push(temparray);
+    }
+    return result;
   }
 
 }
